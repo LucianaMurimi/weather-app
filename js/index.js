82f5b01de6497f0f51873a5f5bbd6c2b73aa1147
technical <technical@silicon-savannah.com>
2020-12-08 14:22:04 +0300

    menu Icon, date&time

--- js/index.js
+++ js/index.js
@@ -13,19 +13,70 @@ function getWeatherInfo (query) {
     })
     .then(/*function to display search results will be here*/)
 }
-
-//TIME
+/*-----------------------------------------------------------------------------
+LOCATION*/
+function getLocation() {
+  /*
+  Check if Geolocation is supported
+    If supported, run the getCurrentPosition() method. If not, display an error message to the user
+    If the getCurrentPosition() method is successful, it returns a coordinates object to the function specified in the parameter (showPosition)
+    Finally, the showPosition() function outputs the Latitude and Longitude
+  */
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else { 
+    let err = "Geolocation is not supported by this browser.";
+  }
+}
+function showPosition(position) {
+  console.log(position.coords.latitude);
+  console.log(position.coords.longitude);
+}
+/*-----------------------------------------------------------------------------
+DATE & TIME*/
 function getDateTime() {
   let dateTime = new Date();
   document.getElementById('date').innerHTML = (`${dateTime.toDateString()}`);
   document.getElementById('time').innerHTML = (`${dateTime.toLocaleTimeString()}`);
 }
+/*-----------------------------------------------------------------------------
+MENU ICON*/
+function listenForMenuIcon(){
+  var menuIcon = document.getElementsByClassName("menu-icon");
+
+  var menuIconFunction = function() {
+      document.getElementById("menu-icon-options-id").classList.toggle("show");
+  };
+
+  for (let i = 0; i < menuIcon.length; i++) {
+      menuIcon[i].addEventListener('click', menuIconFunction);
+  }
+
+  // Close the dropdown menu if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.menu-icon')) {
+	    var dropdowns = document.getElementsByClassName("menu-icon-options");
+	    
+	    for (var i = 0; i < dropdowns.length; i++) {
+	      var openDropdown = dropdowns[i];
+	      if (openDropdown.classList.contains('show')) {
+	        openDropdown.classList.remove('show');
+	      }
+	    }
+	  }
+  }
+}
+/*-----------------------------------------------------------------------------*/
 
 document.addEventListener('DOMContentLoaded', () => {
 //1. Get the device location:
+getLocation();
 //2. Get the date & time:
 getDateTime();
 setInterval(getDateTime, 1000);
+
 //3. Get the weather:
 
+//4. Listen for menu Icon Click
+listenForMenuIcon();
 });
